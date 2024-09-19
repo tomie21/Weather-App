cce961744d342f5664a1730be87ee682fea26dac
Tomie Allen <142113355+tomie21@users.noreply.github.com>
2024-09-19 11:59:16 -0400

    Add files via upload
    simple code of the weather app. User can enter a city, and it will return the temperature and weather condition.

---
+++ app.py
@@ -0,0 +1,18 @@
+import requests
+
+api_key = 'e2a16aab7b0aae12008a1308aee654c8'
+
+ 
+user_input = input("Enter City:  ")
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+if weather_data.json()['cod'] == '404':
+    print("No City Found. Please Try Again")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp}°F")
